011bf76757e2b07da796d1b84a0aede181f769ff
Rona C <rona.chen@hotmail.com>
2023-06-05 01:22:40 -0400

    added code to addrole function, and added role prompt questions in the questions.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2/promise');
 const table = require('console.table');
-const { menu, department } = require('./questions.js');
+const { menu, department, role } = require('./questions.js');
 
 async function main() {
     try {
@@ -30,7 +30,7 @@ async function main() {
                 addDepartment(db);
                 break;
             case 'Add a Role':
-                addRole();
+                addRole(db);
                 break;
             case 'Add an Employee':
                 addEmployee();
@@ -49,27 +49,41 @@ async function main() {
 };
 
 async function viewDepartments(db) {
-    [rows, fields] = await db.query('SELECT id AS ID, name AS DEPARTMENT FROM department')
+    [rows, fields] = await db.query('SELECT id AS ID, name AS DEPARTMENT FROM department');
     console.table(rows);
-    main();
+    await main();
 }
 
 async function viewRoles(db) {
-    [rows, fields] = await db.query('SELECT r.id AS ID, r.title AS ROLE, r.salary AS SALARY, d.name AS DEPARTMENT FROM role AS r LEFT JOIN department AS d ON r.department_id = d.id')
+    [rows, fields] = await db.query('SELECT r.id AS ID, r.title AS ROLE, r.salary AS SALARY, d.name AS DEPARTMENT FROM role AS r LEFT JOIN department AS d ON r.department_id = d.id');
     console.table(rows);
-    main();
+    await main();
 }
 
 async function viewEmployees(db) {
-    [rows, fields] = await db.query('SELECT e.id AS ID, e.first_name AS FIRSTNAME, e.last_name AS LASTNAME, r.title AS POSITION, d.name AS DEPARTMENT, CONCAT(m.first_name, " ", m.last_name) AS MANAGER FROM employee AS e LEFT JOIN role AS r ON e.role_id = r.id INNER JOIN department AS d ON r.department_id = d.id LEFT JOIN employee AS m ON e.manager_id = m.id')
+    [rows, fields] = await db.query('SELECT e.id AS ID, e.first_name AS FIRSTNAME, e.last_name AS LASTNAME, r.title AS POSITION, d.name AS DEPARTMENT, CONCAT(m.first_name, " ", m.last_name) AS MANAGER FROM employee AS e LEFT JOIN role AS r ON e.role_id = r.id INNER JOIN department AS d ON r.department_id = d.id LEFT JOIN employee AS m ON e.manager_id = m.id');
     console.table(rows);
-    main();
+    await main();
 }
 
 async function addDepartment(db) {
     const data = await inquirer.prompt(department);
-    db.query(`INSERT INTO department (name) VALUES ('${data.departmentName}')`);
-    viewDepartments(db);
+    [rows, fields] = await db.query(`INSERT INTO department (name) VALUES ('${data.departmentName}')`);
+    await viewDepartments(db);
+}
+
+async function addRole(db) {
+    const [departments] = await db.query('SELECT * FROM department');
+
+    role.find(question => question.name === 'roleDepartment').choices = departments.map(department => department.name);
+
+    const data = await inquirer.prompt(role);
+
+    let roleDepartmentId = departments.find(department => department.name === data.roleDepartment).id
+
+    await db.query(`INSERT INTO role (title, salary, department_id) VALUES ('${data.roleName}', ${data.salary}, ${roleDepartmentId})`);
+    
+    await viewRoles(db);
 }
 
 main();
--- questions.js
+++ questions.js
@@ -1,17 +1,40 @@
 const menu = [
     {
-        type:'list',
-        message:'What would you like to do?',
-        choices:['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Exit'],
-        name:'menuOption'
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Exit'],
+        name: 'menuOption'
     }
 ]
 
 const department = [
     {
-        type:'input',
-        message:'Enter department name:',
-        name:'departmentName'
+        type: 'input',
+        message: 'Enter department name:',
+        name: 'departmentName'
     }
 ]
-module.exports = {menu, department};
+
+const role = [
+    {
+        type: 'input',
+        message: 'Enter job title:',
+        name: 'roleName'
+    },
+    {
+        type: 'input',
+        message: 'Enter salary:',
+        name: 'salary',
+        validate: input => {
+            if (isNaN(input) === false) return true;
+            return "Please enter a valid number";
+        }
+    },
+    {
+        type: 'list',
+        message: 'Select the department it belongs to:',
+        choices: [],
+        name: 'roleDepartment'
+    }
+]
+module.exports = { menu, department, role };
