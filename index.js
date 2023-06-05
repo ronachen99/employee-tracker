46592800f75f74f6ed3a967cf24ccdc859c2eae6
Rona C <rona.chen@hotmail.com>
2023-06-04 22:30:02 -0400

    added an exit option to end the application, and added a package to make table in console clear for the user to see and added code for view tables

--- index.js
+++ index.js
@@ -1,56 +1,60 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
-const options = require('./options.js')
+const mysql = require('mysql2/promise');
+const options = require('./options.js');
+const table = require('console.table')
 
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: 'tuzichen',
-    database: 'awesome_laboratory_db'
-  },
-  console.log(`╰[⎚ ◡ ⎚]╯ Trying to connect to the awesome_laboratory_db database.`)
-);
-
-db.connect((error) => {
-    if (error) {
-        console.log(`୧(ಥ ⌓ ̅ಥ)୨ ${error.message}`);
-    } 
-    else {
-        console.log (`ᕙ[⎚ ◡ ⎚]ᕗ Connected to the awesome_laboratory_db database.`);
-        userOption();
-    }
-}
-)
-
-const userOption = async () => {
+async function main() {
     try {
+        const db = await mysql.createConnection(
+            {
+                host: 'localhost',
+                user: 'root',
+                password: 'tuzichen',
+                database: 'awesome_laboratory_db'
+            },
+            console.log(`╰[⎚ ◡ ⎚]╯ Main Menu:`)
+        );
+
+        let [rows, fields] = [];
+
         const data = await inquirer.prompt(options);
         switch (data.option) {
             case 'View all Departments':
-                viewDepartments();
-            break;
+                [rows, fields] = await db.query('SELECT id AS ID, name AS DEPARTMENT FROM department')
+                console.table(rows);
+                main();
+                break;
             case 'View all Roles':
-                viewRoles();
-            break;
+                [rows, fields] = await db.query('SELECT r.id AS ID, r.title AS ROLE, r.salary AS SALARY, d.name AS DEPARTMENT FROM role AS r LEFT JOIN department AS d ON r.department_id = d.id')
+                console.table(rows);
+                main();
+                break;
             case 'View all Employees':
-                viewEmployees();
-            break;
+                [rows, fields] = await db.query('SELECT e.id AS ID, e.first_name AS FIRSTNAME, e.last_name AS LASTNAME, r.title AS POSITION, d.name AS DEPARTMENT, CONCAT(m.first_name, " ", m.last_name) AS MANAGER FROM employee AS e LEFT JOIN role AS r ON e.role_id = r.id INNER JOIN department AS d ON r.department_id = d.id LEFT JOIN employee AS m ON e.manager_id = m.id')
+                console.table(rows); 
+                main(); break;
             case 'Add a Department':
                 addDepartment();
-            break;
+                break;
             case 'Add a Role':
                 addRole();
-            break;
+                break;
             case 'Add an Employee':
                 addEmployee();
-            break;
+                break;
             case 'Update an Employee Role':
                 updateRole();
-            break;
+                break;
+            case 'Exit':
+                return process.exit();
         }
-    } catch (error) {
-        console.log(error);
-    }
-}
 
+    } catch (error) {
+        console.log(`୧(ಥ ⌓ ̅ಥ)୨ ${error.message}`);
+        return main();
+    }
+};
+
+main();
+
+// concat(first_name," ", last_name) as MANAGER 
--- options.js
+++ options.js
@@ -2,7 +2,7 @@ const options = [
     {
         type:'list',
         message:'What would you like to do?',
-        choices:['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
+        choices:['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Exit'],
         name:'option'
     }
 ]
