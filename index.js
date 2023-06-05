16a5dcef78fc7cac977dbc40d3b314dac16f62b1
Rona C <rona.chen@hotmail.com>
2023-06-04 23:11:56 -0400

    added another array of prompts in the questions.js and added code that adds a new department

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2/promise');
-const options = require('./options.js');
-const table = require('console.table')
+const table = require('console.table');
+const { menu, department } = require('./questions.js');
 
 async function main() {
     try {
@@ -12,29 +12,22 @@ async function main() {
                 password: 'tuzichen',
                 database: 'awesome_laboratory_db'
             },
-            console.log(`╰[⎚ ◡ ⎚]╯ Main Menu:`)
+            console.log(`╰[⎚ ◡ ⎚]╯ Connected to the Awesome Laboratory Database`)
         );
 
-        let [rows, fields] = [];
-
-        const data = await inquirer.prompt(options);
-        switch (data.option) {
+        const data = await inquirer.prompt(menu);
+        switch (data.menuOption) {
             case 'View all Departments':
-                [rows, fields] = await db.query('SELECT id AS ID, name AS DEPARTMENT FROM department')
-                console.table(rows);
-                main();
+                viewDepartments(db);
                 break;
             case 'View all Roles':
-                [rows, fields] = await db.query('SELECT r.id AS ID, r.title AS ROLE, r.salary AS SALARY, d.name AS DEPARTMENT FROM role AS r LEFT JOIN department AS d ON r.department_id = d.id')
-                console.table(rows);
-                main();
+                viewRoles(db);
                 break;
             case 'View all Employees':
-                [rows, fields] = await db.query('SELECT e.id AS ID, e.first_name AS FIRSTNAME, e.last_name AS LASTNAME, r.title AS POSITION, d.name AS DEPARTMENT, CONCAT(m.first_name, " ", m.last_name) AS MANAGER FROM employee AS e LEFT JOIN role AS r ON e.role_id = r.id INNER JOIN department AS d ON r.department_id = d.id LEFT JOIN employee AS m ON e.manager_id = m.id')
-                console.table(rows); 
-                main(); break;
+                viewEmployees(db);
+                break;
             case 'Add a Department':
-                addDepartment();
+                addDepartment(db);
                 break;
             case 'Add a Role':
                 addRole();
@@ -55,6 +48,28 @@ async function main() {
     }
 };
 
-main();
+async function viewDepartments(db) {
+    [rows, fields] = await db.query('SELECT id AS ID, name AS DEPARTMENT FROM department')
+    console.table(rows);
+    main();
+}
 
-// concat(first_name," ", last_name) as MANAGER 
+async function viewRoles(db) {
+    [rows, fields] = await db.query('SELECT r.id AS ID, r.title AS ROLE, r.salary AS SALARY, d.name AS DEPARTMENT FROM role AS r LEFT JOIN department AS d ON r.department_id = d.id')
+    console.table(rows);
+    main();
+}
+
+async function viewEmployees(db) {
+    [rows, fields] = await db.query('SELECT e.id AS ID, e.first_name AS FIRSTNAME, e.last_name AS LASTNAME, r.title AS POSITION, d.name AS DEPARTMENT, CONCAT(m.first_name, " ", m.last_name) AS MANAGER FROM employee AS e LEFT JOIN role AS r ON e.role_id = r.id INNER JOIN department AS d ON r.department_id = d.id LEFT JOIN employee AS m ON e.manager_id = m.id')
+    console.table(rows);
+    main();
+}
+
+async function addDepartment(db) {
+    const data = await inquirer.prompt(department);
+    db.query(`INSERT INTO department (name) VALUES ('${data.departmentName}')`);
+    viewDepartments(db);
+}
+
+main();
--- questions.js
+++ questions.js
@@ -1,10 +1,17 @@
-const options = [
+const menu = [
     {
         type:'list',
         message:'What would you like to do?',
         choices:['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Exit'],
-        name:'option'
+        name:'menuOption'
     }
 ]
 
-module.exports = options;
+const department = [
+    {
+        type:'input',
+        message:'Enter department name:',
+        name:'departmentName'
+    }
+]
+module.exports = {menu, department};
